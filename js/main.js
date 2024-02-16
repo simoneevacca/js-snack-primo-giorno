37fbafc31d0813f23ff08ca185490b0d553fe17f
simoneevacca <v.simone.vacca@gmail.com>
2024-02-16 14:49:17 +0100

    add ex 2

--- js/main.js
+++ js/main.js
@@ -4,3 +4,21 @@ const number = 100; // number
 console.log(number);
 
 ////////////////////////////////////////////////////////////////////
+
+// Esercizio 2
+
+const PI = 3.14;
+console.log(PI);
+
+// 2/B
+/* PI = 3.1416; */ // const è una costante e non può cambiare il suo valore, vedremo un errore sulla console
+
+// 2/c
+const radius = 8; //number
+let circle = 0; //number
+circle = (radius * 2 * PI);
+console.log(circle);
+
+// Esercizio 3
+
+
